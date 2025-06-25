62a5c731748b3c8301286cd6c71eb9b525b5841a
Aarshia Rawat <aarshia.rawat@gmail.com>
2025-06-25 18:11:16 +0530

    Update api.ts

--- src/components/api.ts
+++ src/components/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
-  withCredentials: true,
+  withCredentials: true, // needed for cookies
 });
 
 api.interceptors.request.use((config) => {
@@ -13,6 +13,5 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
-
 
 export default api;
